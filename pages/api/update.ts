c2cefed38a7694e3ca256fb9860a6dd10fe0e08f
PPuslecki <ppuslecki@reference.be>
2022-07-07 12:30:05 +0200

    try 123

--- pages/api/update.ts
+++ pages/api/update.ts
@@ -9,11 +9,13 @@ export default function handler(
   response: NextApiResponse
 ) {
   const storyblokReqData: StoryblokPayload = req.body;
+
   const ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID;
   const ALGOLIA_API_ADMIN_TOKEN = process.env.ALGOLIA_API_ADMIN_TOKEN;
   const ALGOLIA_INDEX_NAME = process.env.ALGOLIA_INDEX_NAME;
   const STORYBLOK_CONTENT_DELIVERY_API_TOKEN =
     process.env.STORYBLOK_CONTENT_DELIVERY_API_TOKEN;
+
   const algolia = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_ADMIN_TOKEN);
   const storyblok = new StoryblokClient({
     accessToken: STORYBLOK_CONTENT_DELIVERY_API_TOKEN,
@@ -22,19 +24,25 @@ export default function handler(
       type: 'memory',
     },
   });
+
   response.status(200).json({});
 
-  storyblok
-    .getStory(storyblokReqData.story_id.toString())
-    .then(res => {
-      const index = algolia.initIndex(ALGOLIA_INDEX_NAME);
-      const mappedItem = mapStoryblokItem(res.data.story);
-      index
-        .saveObject(mappedItem)
-        .wait()
-        .catch(e => console.log(e));
-      console.log('saved');
-    })
-    .catch(e => console.log(e));
-  return;
+  try {
+    storyblok
+      .getStory(storyblokReqData.story_id.toString())
+      .then(res => {
+        const index = algolia.initIndex(ALGOLIA_INDEX_NAME);
+        const mappedItem = mapStoryblokItem(res.data.story);
+        index
+          .saveObject(mappedItem)
+          .wait()
+          .catch(e => console.log(e));
+        console.log('saved');
+      })
+      .catch(e => console.log(e));
+  } catch (err) {
+    console.error(err);
+    response.status(400).json({});
+  }
+  response.status(200).json({});
 }
