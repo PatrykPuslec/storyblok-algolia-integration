fa20e7f0428fa9281392614a8843752008155a27
PPuslecki <ppuslecki@reference.be>
2022-07-25 10:06:29 +0200

    Schema

--- utils/mapStoryblokItems.ts
+++ utils/mapStoryblokItems.ts
@@ -1,5 +1,10 @@
-import { StoryData } from 'storyblok-js-client';
+import StoryblokClient, { StoryData } from 'storyblok-js-client';
+const STORYBLOK_CONTENT_DELIVERY_API_TOKEN =
+  process.env.STORYBLOK_CONTENT_DELIVERY_API_TOKEN;
 
+const storyblok = new StoryblokClient({
+  accessToken: STORYBLOK_CONTENT_DELIVERY_API_TOKEN,
+});
 export type mappedStoryblokItem = {
   objectID: string;
   story_id: number;
@@ -13,28 +18,72 @@ export type mappedStoryblokItem = {
   searchableContent?: Array<string>;
 };
 
-const getTextFieldData = (item, searchableContent: string[]) => {
-  if (item.text && typeof item.text === 'string') {
-    searchableContent.push(item.text);
+export type componentSchema = {
+  name: string;
+  display_name: string | null;
+  id: number;
+  created_at: string;
+  updated_at: string;
+  schema: {
+    [name: string]: {
+      type: string;
+    };
+  };
+};
+
+const searchableContentTypes = ['text', 'richtext', 'textarea'];
+
+const getTextFieldData = (
+  item,
+  searchableContent: string[],
+  schema: componentSchema[]
+) => {
+  const currentItemSchema = schema.find(
+    schemaItem => item.component === schemaItem.name
+  );
+
+  if (currentItemSchema && currentItemSchema.schema) {
+    const { schema } = currentItemSchema;
+
+    Object.keys(item).forEach(itemKey => {
+      if (
+        schema[itemKey] &&
+        schema[itemKey].type &&
+        searchableContentTypes.includes(schema[itemKey].type)
+      ) {
+        if (schema[itemKey].type === 'richtext') {
+          const richtextResolver = storyblok.richTextResolver.render(
+            item[itemKey]
+          );
+          searchableContent.push(richtextResolver);
+        } else {
+          searchableContent.push(item[itemKey]);
+        }
+      }
+    });
   }
-  if (item.title) {
-    searchableContent.push(item.title);
-  }
+  // if (item.text && typeof item.text === 'string') {
+  //   searchableContent.push(item.text);
+  // }
+
   if (typeof item.text === 'object' && item.text.content) {
-    getTextFieldData(item.text.content[0], searchableContent);
+    getTextFieldData(item.text.content[0], searchableContent, schema);
   } else if (item.content) {
-    getTextFieldData(item.content[0], searchableContent);
+    getTextFieldData(item.content[0], searchableContent, schema);
   }
   return;
 };
 
-export const mapStoryblokItem = (item: StoryData): mappedStoryblokItem => {
+export const mapStoryblokItem = (
+  item: StoryData,
+  schema: any
+): mappedStoryblokItem => {
   if (!item) return null;
   const searchableContent = [];
 
   if (item.content && item.content.body) {
     item.content.body.forEach(element => {
-      getTextFieldData(element, searchableContent);
+      getTextFieldData(element, searchableContent, schema);
     });
   }
 
